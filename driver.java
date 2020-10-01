950af722dda7cfc5bc6778350d60f27798ed75eb
Jake <jake.peterson7@aol.com>
2020-09-30 21:52:17 -0500

    Setup strings and int[] for the key and plaintext
    The binary values will go into the int[]s.

--- driver.java
+++ driver.java
@@ -1,5 +1,11 @@
 public class driver {
     public static void main(String[] args) {
+        String keyAlpha = "abcde";
+        String plainText = "Hello";
+
+        int[] keyBin = new int[35];
+        int[] plainBin = new int[35];
         
+
     }
 }
