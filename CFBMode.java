9b69807ff818c111875db83d4083418b14e928db
Jake <jake.peterson7@aol.com>
2020-10-02 18:57:23 -0500

    Create extendedStringToBinaryArray method
    This is used to create the plainBin array.

--- CFBMode.java
+++ CFBMode.java
@@ -52,5 +52,40 @@ public class CFBMode {
 
         return cipherText;
     }
+    
+    public static int[] extendedStringToBinaryArray(String string) {
+
+        int[] result = new int[string.length()*7];
+
+        int counter = 0; // used to keep track of position in the key array.
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i); // get a char from the String.
+            String binString = Integer.toBinaryString(c); // bin holds the binary value of the first char as a string.
+            String[] bitArray = binString.split(""); // split binString into a string array.
+            //System.out.println("The length of " + i + " is: " + bitArray.length);
+            // printStringArray(bitArray);
+
+            // need to add padding if bitArray.length is < 7
+            if (bitArray.length < 7) {
+                String[] temp = bitArray; // temp holds original values
+                bitArray = new String[7]; // bitArray is made to be bigger (from 6 to 7)
+                for (int j = 0; j < (bitArray.length - temp.length);j++){
+                    bitArray[j] = "0";
+                }
+                int n = 0;
+                for (int p = (bitArray.length - temp.length); p < bitArray.length; p++) {
+                    bitArray[p] = temp[n];
+                    n++;
+                }
+            }
+
+            // add values to the array.
+            for (int k = 0; k < bitArray.length; k++) {
+                result[counter] = Integer.parseInt(bitArray[k]);
+                counter++;
+            }
+        }
+        return result;
+    }
 
 }
