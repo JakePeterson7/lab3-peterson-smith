d79a967887c4e3785f600bcebe0213fca0ebd8e8
Jake <jake.peterson7@aol.com>
2020-10-09 19:09:36 -0500

    Add in Decryption for the block cipher
    This is checked to work in the classes main method.

--- BlockCipher.java
+++ BlockCipher.java
@@ -3,34 +3,27 @@ import java.util.Arrays;
 public class BlockCipher {
 
     public static int[] Encrypt(int[] text, int[] key) {
-        // String keyAlpha = key;
-        // String plainText = text;
-        // System.out.println(keyAlpha);
-        // System.out.println(plainText);
 
         int[] keyBin = key;
         int[] plainBin = text;
 
-        // stringToBinaryArray(keyAlpha, keyBin);
-        // stringToBinaryArray(plainText, plainBin);
+        circleRotateThree(plainBin);// this has been checked to work
 
-        // perform circular shift of three bits on the plaintext (plainBin).
-
-        // test rotation
-        // int[] test = new int[] { 1, 2, 3, 4, 5, 6, 7 };
-        // printArray(test);
-        // circleRotateThree(test);
-        // printArray(test);
-
-        // printIntArray(plainBin);
-        circleRotateThree(plainBin);
-        // add keyBin to plainBin (bitwise)
-        int[] result = addBinaryArrays(plainBin, keyBin);
-       // printIntArray(result);
-
-       return result;
+        int[] result = addBinaryArrays(plainBin, keyBin); // this has been checked to work
+   
+        return result;
 
     }// end main
+
+    // reverse the encryption
+    public static int[] Decrypt(int[] encryption, int[] keyBin) {
+
+        int[] result = addBinaryArrays(encryption, keyBin);
+
+        reverseCircleRotate(result);
+
+        return result;// this gives correct result, I check by hand.
+    }
 
     private static void circleRotateThree(int[] ar) {
         for (int i = 0; i < 3; i++) {
@@ -41,6 +34,17 @@ public class BlockCipher {
             }
 
             ar[0] = last;
+        }
+    }
+
+    private static void reverseCircleRotate(int[] ar) {
+        for (int i = 0; i < 3; i++) {
+            int first = ar[0];
+
+            for (int k = 0; k < ar.length - 1; k++) {
+                ar[k] = ar[k + 1];
+            }
+            ar[ar.length - 1] = first;
         }
     }
 
@@ -57,14 +61,14 @@ public class BlockCipher {
             char c = string.charAt(i); // get a char from the String.
             String binString = Integer.toBinaryString(c); // bin holds the binary value of the first char as a string.
             String[] bitArray = binString.split(""); // split binString into a string array.
-           // System.out.println("The length of " + i + " is: " + bitArray.length);
+            // System.out.println("The length of " + i + " is: " + bitArray.length);
             // printStringArray(bitArray);
 
             // need to add padding if bitArray.length is < 7
             if (bitArray.length < 7) {
                 String[] temp = bitArray; // temp holds original values
                 bitArray = new String[7]; // bitArray is made to be bigger (from 6 to 7)
-                for (int j = 0; j < (bitArray.length - temp.length);j++){
+                for (int j = 0; j < (bitArray.length - temp.length); j++) {
                     bitArray[j] = "0";
                 }
                 int n = 0;
@@ -95,18 +99,19 @@ public class BlockCipher {
         System.out.println();
     }
 
-    public static int[] addBinaryArrays(int[] plainBin, int[] keyBin){
+    public static int[] addBinaryArrays(int[] plainBin, int[] keyBin) {
+
         int[] result = new int[35];
-        for(int i = 0; i<35; i++){
+        for (int i = 0; i < 35; i++) {
             result[i] = addBits(plainBin[i], keyBin[i]);
         }
         return result;
     }
 
-    public static int addBits(int a, int b){
-        if(a == b){
+    public static int addBits(int a, int b) {
+        if (a == b) {
             return 0;
-        }else
+        } else
             return 1;
     }
 
@@ -116,4 +121,32 @@ public class BlockCipher {
         }
         System.out.println();
     }
+
+    // Testing encryption and decryption.
+    public static void main(String[] args) {
+        String keyAlpha = "a5Z#xa5z#x";
+        String plainText = "HelloHello";
+        int[] keyBin = BlockCipher.stringToBinaryArray(keyAlpha);
+        int[] plainBin = BlockCipher.stringToBinaryArray(plainText);
+
+        System.out.println("plainBin: ");
+        BlockCipher.printArray(plainBin);
+
+        System.out.println("keyBin: ");
+        BlockCipher.printArray(keyBin);
+
+        System.out.println("add arrays: ");
+        BlockCipher.printArray(addBinaryArrays(plainBin, keyBin));
+
+        int[] encryption = Encrypt(plainBin, keyBin);
+
+        System.out.println("encryption: ");
+        BlockCipher.printArray(encryption);
+
+        int[] decryption = Decrypt(encryption, keyBin);
+
+        System.out.println("decryption: ");
+        BlockCipher.printArray(decryption);
+    }
+
 }
