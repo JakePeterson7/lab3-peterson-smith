baa40cfbb67abcfc3b3c9f78daa10325b44e83fc
JayS <smit9025@morris.umn.edu>
2020-10-13 16:42:34 -0500

    Fixed how CBC does encryption and decryption because I misunderstood what it did. It now properly goes through each block and encrypts/decrypts while bringing over content from one block to the next. Before it basically acted like ECB.

--- CBCMode.java
+++ CBCMode.java
@@ -1,59 +1,87 @@
 import static java.util.Arrays.copyOfRange;
 
 class CBCMode {
+    private static int[] XORtext =new int[35];
+    private static int cryptNum;
+
     public int[] cipher(String key, String text, int crypt, String iv) {
         int[] keyBin = BlockCipher.stringToBinaryArray(key);
         int[] plainBin = BlockCipher.stringToBinaryArray(text);
         int[] cipherText = new int[plainBin.length];
         int[] IV = BlockCipher.stringToBinaryArray(iv);
+        cryptNum = crypt;
         System.out.println("IV: " + BlockCipher.BinaryArrayToString(IV));
-        if (crypt == 0) {
-            encrypt(keyBin, plainBin, IV, cipherText);
-        } else {
+        if (cryptNum == 1) {
             decrypt(keyBin, plainBin, IV, cipherText);
+        } else {
+            encrypt(keyBin, plainBin, IV, cipherText);
         }
         return cipherText;
     }
 
     public static void encrypt(int[] keyBin, int[] plainBin, int[] IV, int[] cipherText) {
-        int[] temp;
-        int[] tempBin;
-        int l = plainBin.length;
-        for (int i = 0; i < l / 35; i++) {
-            tempBin = copyOfRange(plainBin, (i * 35), ((i * 35) + 35));
-            int[] newInput = BlockCipher.addBinaryArrays(tempBin, IV);
-            temp = BlockCipher.Encrypt(newInput, keyBin);
-            System.arraycopy(temp, 0, cipherText, (i * 35), temp.length);
-        }
-        if (l % 35 != 0) {
-            tempBin = copyOfRange(plainBin, (l - l % 35), l);
-            int[] newInput = BlockCipher.addBinaryArrays(tempBin, IV);
-            temp = BlockCipher.Encrypt(newInput, keyBin);
-            System.arraycopy(temp, 0, cipherText, (l - l % 35), temp.length);
-        }
+        int[] temp; //Holds encrypted block
+        int[] tempBin; //Holds plaintext to be encrypted
+        int l = plainBin.length; //Readability
+        initialEncryption(keyBin, plainBin, IV, cipherText); //Initial run of encryption
+            for (int i = 1; i < l / 35; i++) { //For each block after the first
+                tempBin = copyOfRange(plainBin, (i * 35), ((i * 35) + 35)); //Copy plaintext block into tempBin
+                int[] newInput = BlockCipher.addBinaryArrays(tempBin, XORtext); //XOR plaintext and saved XORtext
+                temp = BlockCipher.Encrypt(newInput, keyBin); //Encrypt result of XOR with key
+                System.arraycopy(temp, 0, cipherText, (i * 35), 35); //Copy result to resulting ciphertext
+                System.arraycopy(temp, 0, XORtext, 0, 35); //Overwrite XORtext with result for further encryption
+            }
+            if (l % 35 != 0) { //Handles overflow
+                tempBin = copyOfRange(plainBin, (l - l % 35), l);
+                int[] newInput = BlockCipher.addBinaryArrays(tempBin, XORtext);
+                temp = BlockCipher.Encrypt(newInput, keyBin);
+                System.arraycopy(temp, 0, cipherText, (l - l % 35), temp.length);
+            }
+    }
+
+    public static void initialEncryption(int[] keyBin, int[] plainBin, int[] IV, int[] cipherText) {
+        int[] temp; //Holds encrypted block
+        int[] tempBin; //Holds plaintext to be encrypted
+        tempBin = copyOfRange(plainBin, 0,  35); //Copy plaintext block into tempBin
+        int[] newInput = BlockCipher.addBinaryArrays(tempBin, IV); //XOR plaintext and IV
+        temp = BlockCipher.Encrypt(newInput, keyBin); //Encrypt result of XOR with key
+        System.arraycopy(temp, 0, XORtext, 0, temp.length); //Copy result of encrypt for future XORs
+        System.arraycopy(temp, 0, cipherText, 0, temp.length); //Copy result to resulting ciphertext
     }
 
     public static void decrypt(int[] keyBin, int[] plainBin, int[] IV, int[] cipherText) {
-        int[] temp;
-        int[] tempBin;
-        int l = plainBin.length;
-        for (int i = 0; i < l / 35; i++) {
-            tempBin = copyOfRange(plainBin, (i * 35), ((i * 35) + 35));
-            temp = BlockCipher.Decrypt(tempBin, keyBin);
-            int[] newInput = BlockCipher.addBinaryArrays(temp, IV);
-            System.arraycopy(newInput, 0, cipherText, (i * 35), temp.length);
-        }
-        if (l % 35 != 0) {
-            tempBin = copyOfRange(plainBin, (l - l % 35), l);
-            temp = BlockCipher.Decrypt(tempBin, keyBin);
-            int[] newInput = BlockCipher.addBinaryArrays(temp, IV);
-            System.arraycopy(newInput, 0, cipherText, (l - l % 35), temp.length);
-        }
+        int[] temp; //Holds encrypted block
+        int[] tempBin; //Holds plaintext to be encrypted
+        int l = plainBin.length; //Readability
+        initialDecryption(keyBin, plainBin, IV, cipherText); //Initial run of decryption
+            for (int i = 1; i < l / 35; i++) { //For each block after the first
+                tempBin = copyOfRange(plainBin, (i * 35), ((i * 35) + 35)); // Copy ciphertext block into tempBin
+                temp = BlockCipher.Decrypt(tempBin, keyBin); //Decrypt ciphertext with key
+                int[] newInput = BlockCipher.addBinaryArrays(temp, XORtext); //XOR result of decrption with last block ciphertext
+                System.arraycopy(newInput, 0, cipherText, (i * 35), temp.length); //Copy result of decrypt to resulting plaintext
+                System.arraycopy(tempBin, 0, XORtext, 0, 35); //Copy ciphertext from this block for next block
+            }
+            if (l % 35 != 0) {
+                tempBin = copyOfRange(plainBin, (l - l % 35), l);
+                temp = BlockCipher.Decrypt(tempBin, keyBin);
+                int[] newInput = BlockCipher.addBinaryArrays(temp, XORtext);
+                System.arraycopy(newInput, 0, cipherText, (l - l % 35), temp.length);
+            }
+    }
+
+    public static void initialDecryption(int[] keyBin, int[] plainBin, int[] IV, int[] cipherText) {
+        int[] temp; //Holds encrypted block
+        int[] tempBin; //Holds plaintext to be encrypted
+        System.arraycopy(plainBin, 0, XORtext, 0, 35); //Copy ciphertext block for next block
+        tempBin = copyOfRange(plainBin, 0,  35); //Copy ciphertext block into tempBin
+        int[] newInput = BlockCipher.Decrypt(tempBin, keyBin); //Decrypt result of XOR with key
+        temp = BlockCipher.addBinaryArrays(newInput, IV); //XOR ciphertext with IV
+        System.arraycopy(temp, 0, cipherText, 0, temp.length); //Copy result of decrypt to resulting plaintext
     }
 
     public static void main(String[] args) { //Test encrypt and decrypt
-        String text = "yikesyikes";
-        String key = "hellohello";
+        String text = "yikesyikesyikes";
+        String key = "hellohellohello";
         String IV = "arere";
         CBCMode CBC = new CBCMode();
         String result = BlockCipher.BinaryArrayToString(CBC.cipher(key, text, 0, IV));
